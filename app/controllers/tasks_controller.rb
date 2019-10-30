5eb52508c1fb1ed24fe6b0e4e998d389fca894f0
Rutger Taal <rutger@Rutgers-MacBook-Pro.local>
2019-10-30 09:54:02 +0900

    add JS functions to index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,7 +35,7 @@ class TasksController < ApplicationController
 
     if @task.save
       TaskMailer.creation_email(@task).deliver_now
-      SampleJob.perform_later
+      # SampleJob.perform_later
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
@@ -54,7 +54,6 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
